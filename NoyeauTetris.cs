e6d6e73757ca20effaee7f445876e12f72983589
clarapanissetlgdc-collab <clarapanissetlgdc@gmail.com>
2026-03-19 16:51:24 +0100

    Update NoyeauTetris.cs

--- NoyeauTetris.cs
+++ NoyeauTetris.cs
@@ -17,4 +17,55 @@ namespace NoyauTetris
         public static int LargeurGrille = 12;
         public static int HauteurGrille = 15;
     }
+        //Defition de la position d'un carré.
+    public class Position
+    {
+        public int X;
+        public int Y;
+        
+        public Position(int x, int y)
+        {
+            X=x;
+            Y=y;
+        }
+
+        public Position DeplaceGauche()
+        {
+            return new Position(this.X + 1, this.Y);
+        }
+
+        public Position DeplaceDroite()
+        {
+             return new Position(this.X - 1, this.Y);
+        }
+
+        public Position DeplaceBas()
+        {
+             return new Position(this.X, this.Y + 1);
+        }
+    }
+
+    //Definition d'un tableau de quadruplets de positions.
+    public class Tetrino
+    {
+          public static Position[][] TetrinosTab = new Position[][]
+        {
+            // carre
+            new Position[] { new Position(0, 0), new Position(1, 0),
+            new Position(0, -1), new Position(1, -1) },
+            // barre horizontale
+            new Position[] { new Position(0, 0), new Position(1, 0),
+            new Position(2, 0), new Position(3, 0) },
+            // barre verticale
+            new Position[] { new Position(0, 0), new Position(0, -1),
+            new Position(0, -2), new Position(0, -3) }
+        };
+
+        public Position[][] Indice = TetrinosTab;
+        public Position PositionOrigine 
+    }
+
+
+
+}
 }
